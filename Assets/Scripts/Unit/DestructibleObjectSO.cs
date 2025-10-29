8050513ab352a57912e9521dfe081ac64f6f2d42
ETN-xun <781833086@qq.com>
2025-10-29 16:29:39 +0800

    revert

--- Assets/Scripts/Unit/DestructibleObjectSO.cs
+++ Assets/Scripts/Unit/DestructibleObjectSO.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Tilemaps;
 
 [CreateAssetMenu(fileName = "DestructibleObjectSO", menuName = "Game/DestructibleObjectSO")]
 public class DestructibleObjectSO : ScriptableObject
@@ -21,12 +20,4 @@ public class DestructibleObjectSO : ScriptableObject
     
     [Tooltip(("是否激活"))]
     public bool isActive = false;
-    
-    [Header("不同状态的Tile")]
-    public TileBase tileFullHP;      // 满护盾Tile
-    public TileBase tileInactive;    // 未激活Tile（缓存区专用）
-    public TileBase tileDead;        // 死亡Tile
-
-    [Header("死亡延时(秒)")]
-    public float deathDelay = 1.5f;
 }
